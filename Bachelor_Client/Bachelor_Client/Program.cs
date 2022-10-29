96e2d1708df3d04022e3f9324f2050b06c52eba6
roxi2601 <roxi260111@gmail.com>
2022-10-29 14:10:35 +0200

    zz

---
+++ Bachelor_Client/Bachelor_Client/Program.cs
@@ -0,0 +1,17 @@
+using Bachelor_Client;
+using Bachelor_Client.Services.Account;
+using Bachelor_Client.Services.Rest;
+using Bachelor_Client.Services.WorkerConfig;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped<IWorkerConfigService, WorkerConfigService>();
+builder.Services.AddScoped<IRestService, RestService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
