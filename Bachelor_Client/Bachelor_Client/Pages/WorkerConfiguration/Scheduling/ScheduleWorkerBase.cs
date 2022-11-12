e47aed097eb57e118f18f04b4745cee8df01b525
roxi2601 <roxi260111@gmail.com>
2022-11-12 14:14:11 +0100

    schedule view

---
+++ Bachelor_Client/Bachelor_Client/Pages/WorkerConfiguration/Scheduling/ScheduleWorkerBase.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Bachelor_Client.Pages.WorkerConfiguration.Scheduling;
+
+public class ScheduleWorkerBase: ComponentBase
+{
+    protected bool ShowConfirmation { get; set; }
+
+    [Parameter]
+    public string ConfirmationTitle { get; set; } = "Schedule Worker";
+   
+    [Parameter]
+    public int ID { get; set; } 
+
+    [Parameter]
+    public string ConfirmationMessage { get; set; }
+
+    public void Show()
+    {
+        ShowConfirmation = true;
+        StateHasChanged();
+    }
+
+    [Parameter]
+    public EventCallback<bool> ConfirmationChanged { get; set; }
+
+    protected async Task OnConfirmationChange(bool value)
+    {
+        ShowConfirmation = false;
+        await ConfirmationChanged.InvokeAsync(value);
+    }
+}
