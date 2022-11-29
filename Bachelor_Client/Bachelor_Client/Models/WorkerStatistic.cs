c135a6c62dad8afd4522c197ab02d84785656e0c
Cata011 <alex_catalin1700@yahoo.com>
2022-11-29 19:46:02 +0100

    cc

--- Bachelor_Client/Bachelor_Client/Models/WorkerStatistic.cs
+++ Bachelor_Client/Bachelor_Client/Models/WorkerStatistic.cs
@@ -6,10 +6,10 @@ namespace Bachelor_Client.Models{
     {
         public int PkWorkerStatisticsId { get; set; }
         public int FkWorkerConfigurationId { get; set; }
-        public int? NumberOfSuccesfulRuns { get; set; }
-        public int? NumberOfFailedRuns { get; set; }
-        public DateTime? LastRunTime { get; set; }
-        public decimal? LastRunTimeLengthSec { get; set; }
+        public int NumberOfSuccesfulRuns { get; set; }
+        public int NumberOfFailedRuns { get; set; }
+        public DateTime LastRunTime { get; set; }
+        public decimal LastRunTimeLengthSec { get; set; }
         
         public virtual WorkerConfiguration FkWorkerConfiguration { get; set; } = null!;
     }
