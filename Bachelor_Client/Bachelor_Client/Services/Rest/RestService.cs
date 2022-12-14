69742e86beb0a02ed42bef3c4014fe0e9f9a29ef
Roksana Dziadowicz <roxi260111@gmail.com>
2022-12-14 14:19:35 +0100

    last commit

--- Bachelor_Client/Bachelor_Client/Services/Rest/RestService.cs
+++ Bachelor_Client/Bachelor_Client/Services/Rest/RestService.cs
@@ -2,6 +2,7 @@
 using Aspose.Cells;
 using Aspose.Cells.Utility;
 using Bachelor_Client.Models;
+using Bunit.Extensions;
 using Newtonsoft.Json;
 namespace Bachelor_Client.Services.Rest;
 
@@ -37,7 +38,7 @@ public class RestService : IRestService
         string status;
 
         if (content != null && !content.Equals("The URL is not valid") && !content.Contains("\"StatusCode\":400\"") &&
-            !content.Contains("Invalid URI"))
+            !content.Contains("Invalid URI") && !content.IsNullOrEmpty())
         {
             JsonUtility.ImportData(content, worksheet.Cells, 0, 0, options);
             workbook.Save("Import-Data-JSON-To-Excel.xlsx");
@@ -58,7 +59,7 @@ public class RestService : IRestService
         string status;
 
         if (content != null && !content.Equals("The URL is not valid") && !content.Contains("\"StatusCode\":400\"") &&
-            !content.Contains("Invalid URI"))
+            !content.Contains("Invalid URI") && !content.IsNullOrEmpty())
         {
             JsonUtility.ImportData(content, worksheet.Cells, 0, 0, options);
             workbook.Save("Import-Data-JSON-To-CSV.csv");
